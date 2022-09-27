bb2b6c5d103362d3d60c93747841ba0ece22aefb
LeandroBenitez12 <Leansvae9@gmail.com>
2022-09-27 19:52:24 -0300

    realizando pruebas de la placa

---
+++ Software/Pruebas/Magia.ino
@@ -0,0 +1,125 @@
+#include "DriverDRV8833.h"
+
+//debug
+#define DEBUG 1
+#define TICK_DEBUG 500
+unsigned long tiempo_actual = 0;
+
+// declaramos pines
+// motores
+#define PIN_ENGINE_MR1 23
+#define PIN_ENGINE_MR2 22
+#define PIN_ENGINE_ML1 19
+#define PIN_ENGINE_ML2 18
+
+// Button
+#define PIN_BUTTON_START 39
+bool button_start;
+
+//Sensor final
+#define PIN_SENSOR_FINAL 32
+
+//veocidades motores pwm
+int speedRight = 255;
+int speedLeft = 255;
+int averageSpeed = 255;
+int speedTurn = 255;
+int lec;
+
+//instancio los objetos
+Engine *engineRigh = new  Engine(PIN_ENGINE_MR1, PIN_ENGINE_MR2);
+Engine *engineLeft = new Engine(PIN_ENGINE_ML1, PIN_ENGINE_ML2);
+
+
+// funciones de los motores
+void forward()
+{
+  engineRigh->SetVelocidad(speedRight);
+  engineLeft->SetVelocidad(speedLeft);
+  engineRigh->Forward();
+  engineLeft->Forward();
+}
+
+void backward()
+{
+  engineRigh->SetVelocidad(speedRight);
+  engineLeft->SetVelocidad(speedLeft);
+  engineRigh->Backward();
+  engineLeft->Backward();
+}
+
+void left()
+{
+  engineRigh->SetVelocidad(speedRight);
+  engineLeft->SetVelocidad(speedLeft);
+  engineRigh->Forward();
+  engineLeft->Backward();
+}
+
+void right()
+{
+  engineRigh->SetVelocidad(speedRight);
+  engineLeft->SetVelocidad(speedLeft);
+  engineRigh->Backward();
+  engineLeft->Forward();
+}
+
+void stop()
+{
+  engineRigh->SetVelocidad(0);
+  engineLeft->SetVelocidad(0);
+  engineRigh->Stop();
+  engineLeft->Stop();
+}
+
+// casos de la maquina de estado
+enum movement
+{
+  STANDBY,
+  CONTINUE,
+  STOP,
+  RIGHT_TURN,
+  LEFT_TURN,
+  FULL_TURN,
+  POST_TURN,
+  ANT_TURN
+};
+int movement = STANDBY;
+void switchCase(){
+    switch(movement){
+        case STANDBY:
+            movement = CONTINUE;
+            break;
+        case CONTINUE:
+
+            forward();
+            break;
+        case STOP:
+            stop();
+            break;
+        case RIGHT_TURN :
+            right();
+            break;
+        case LEFT_TURN :
+            left();
+            break;
+        case FULL_TURN :
+            right();
+            break;
+    }
+}
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(32,INPUT);
+}
+
+void loop() 
+{
+    int lec = analogRead(PIN_SENSOR_FINAL);
+    switchCase();
+
+    
+}
--- Software/Pruebas/Pruebas.ino
+++ Software/Pruebas/Pruebas.ino
@@ -1,256 +1,79 @@
-#include <Encoder.h>
-#include <Motor.h>
-
-// declaramos pines
-// motores
-#define PIN_PWM_ENA 15
-#define PIN_MOTOR_MR1 2
-#define PIN_MOTOR_MR2 4
-#define PIN_PWM_ENB 19
-#define PIN_MOTOR_ML1 18
-#define PIN_MOTOR_ML2 5
-
-// ultrasonidos HR04
-#define PIN_SENSOR_DERECHO_ECHO1 35
-#define PIN_SENSOR_ADELANTE_ECHO2 33
-#define PIN_SENSOR_IZQUIERDO_ECHO3 14
-#define PIN_SENSOR_DERECHO_TRIG1 32
-#define PIN_SENSOR_ADELANTE_TRIG2 25
-#define PIN_SENSOR_IZQUIERDO_TRIG3 12
-#define TICK_ULTRASONIDO 10
-unsigned long tiempo_actual = 0;
-unsigned long tiempo_actual_pid = 0;
-int distancia_izquierda;
-int distancia_frontal;
-int distancia_derecha;
-
-// Button
-#define PIN_BUZZER 23
-#define PIN_BUTTON_START 13
-bool boton_start;
-
-//encoders
-#define PIN_ENCODER_DER 23
-#define PIN_ENCODER_IZQ 13
-
-// const
-#define DISTANCIA_MINIMA 200
-#define DISTANCIA_LADOS 200
-#define DISTANCIA_MAXIMA 250
-#define TICK_PID 70
-#define TICK_ULTRASONIDO 10
-#define TICK_NOTIFICACION 500
-#define TICK_STOP 500
-#define TICK_FORWARD 500
-
-// veocidades motores pwm
-int velocidad_derecha = 180;
-int velocidad_izquierda = 180;
-int velocidad_media = 180;
-int velocidad_giro = 175;
-const int PWMChannel1 = 0;
-const int PWMChannel2 = 1;
-
-//instancio los objetos
-
-Motor *MDer = new  Motor(PIN_MOTOR_MR1, PIN_MOTOR_MR2, PIN_PWM_ENB, PWMChannel1);
-Motor *MIzq = new Motor(PIN_MOTOR_ML1, PIN_MOTOR_ML2, PIN_PWM_ENA, PWMChannel2);
-
-/*Ultrasonido *sensor_frontal = new Ultrasonido(PIN_SENSOR_ADELANTE_TRIG2, PIN_SENSOR_ADELANTE_ECHO2);
-Ultrasonido *sensor_derecho = new Ultrasonido(PIN_SENSOR_DERECHO_TRIG1, PIN_SENSOR_DERECHO_ECHO1);
-Ultrasonido *sensor_izquierdo = new Ultrasonido(PIN_SENSOR_IZQUIERDO_TRIG3, PIN_SENSOR_IZQUIERDO_ECHO3);
-
-
-
-Button *start = new Button(PIN_BUTTON_START);
-*/
-Encoder *EncoderDer = new Encoder(PIN_ENCODER_DER);
-Encoder *EncoderIzq = new Encoder(PIN_ENCODER_IZQ);
-
-
-// funciones de los motores
-void Forward()
+/*#define PIN_BUTTON_START 39
+#define PIN_LED 2
+class Buzzer
 {
-  MDer->SetVelocidad(velocidad_derecha);
-  MIzq->SetVelocidad(velocidad_izquierda);
-  MDer->Forward();
-  MIzq->Forward();
-}
-// HACIA ATRAS
-void Backward()
-{
-  MDer->SetVelocidad(velocidad_derecha);
-  MIzq->SetVelocidad(velocidad_izquierda);
-  MDer->Backward();
-  MIzq->Backward();
-}
-// GIRO SOBRE SU PROPIO EJE A LA DERECHA
-void Left()
-{
-  MDer->SetVelocidad(velocidad_derecha);
-  MIzq->SetVelocidad(velocidad_izquierda);
-  MDer->Forward();
-  MIzq->Backward();
-}
-// GIRO SOBRE SU PROPIO EJE A LA IZQUIERDA
-void Right()
-{
-  MDer->SetVelocidad(velocidad_derecha);
-  MIzq->SetVelocidad(velocidad_izquierda);
-  MDer->Backward();
-  MIzq->Forward();
-}
-// PARAR
-void Stop()
-{
-  MDer->SetVelocidad(0);
-  MIzq->SetVelocidad(0);
-  MDer->Stop();
-  MIzq->Stop();
-}
+private:
+  int pin;
 
-//funcion para doblar por grados
-void DoblarDer(int angulo){
-    int giro = EncoderDer->Angle();
-    while(giro <= angulo) {
-      Right();
-      giro = EncoderDer->Angle();
-        }
-    EncoderDer->SetCont(0);
-    }
-
-void DoblarIzq(int angulo){
-    EncoderDer->SetCont(0);
-    int giro = EncoderDer->Angle();
-    while(giro <= angulo) {
-      Left();
-      Serial.println(EncoderDer->Angle());
-      giro = EncoderDer->Angle();
-        }
-    }
-
-// casos de la maquina de estado
-enum movimiento
+public:
+  Buzzer(int p)
+  {
+    pin = p;
+    pinMode(pin, OUTPUT);
+  }
+  void setPrenderLed()
+  {
+    digitalWrite(pin, HIGH);
+  }
+  void setApagarLed()
+  {
+    digitalWrite(pin, LOW);
+  }
+};
+class Button
 {
-  INICIAL,
-  PASILLO,
-  PARED,
-  DESVIO_DERECHA,
-  DESVIO_IZQUIERDA,
-  CALLEJON,
-  POST_DOBLAR,
-  ANT_DOBLAR
+private:
+  int pin;
+
+  // metodo
+public:
+  Button(int p)
+  {
+    pin = p;
+
+    pinMode(pin, INPUT);
+  }
+
+  // metodos o acciones
+  bool getIsPress()
+  {
+    bool estado = digitalRead(pin);
+    return estado;
+  }
 };
 
-int movimiento = INICIAL;
+// Instancio los buttons *punteros
+Button *start = new Button(PIN_BUTTON_START);
+// Instancio los buzzers
+Buzzer *Led = new Buzzer(PIN_LED);
 
-//logica de movimiento
-/*void Movimientos_robot()
+// buzzers
+void LedOn()
 {
-  switch (movimiento)
-  {
-  case INICIAL:
-  {
-    boton_start = start->GetIsPress();
-    if (boton_start)
-    {
-      delay(3000);
-      movimiento = PASILLO;
-    }
-    else
-    {
-      Stop();
-    }
-
-    break;
-  }
-
-  case PASILLO:
-  { 
-    Forward();
-    // cambio de caso a pared
-    if (distancia_frontal < DISTANCIA_MINIMA) movimiento = PARED;
-    if (distancia_izquierda > DISTANCIA_LADOS) movimiento = ANT_DOBLAR;
-
-    break;
-  }
-
-  case PARED:
-  {
-    Stop();
-    delay(TICK_STOP);
-
-    if (distancia_derecha < DISTANCIA_LADOS && distancia_izquierda > DISTANCIA_LADOS) movimiento = DESVIO_IZQUIERDA;
-    if (distancia_derecha > DISTANCIA_LADOS && distancia_izquierda < DISTANCIA_LADOS) movimiento = DESVIO_DERECHA;
-    if (distancia_derecha > DISTANCIA_LADOS && distancia_izquierda > DISTANCIA_LADOS) movimiento = DESVIO_IZQUIERDA;
-    if (distancia_derecha < DISTANCIA_LADOS && distancia_izquierda < DISTANCIA_LADOS) movimiento = CALLEJON;
-
-    break;
-  }
-
-  case DESVIO_DERECHA:
-  {
-    DoblarDer(90);
-    movimiento = POST_DOBLAR;
-    
-    break;
-  }
-
-  case DESVIO_IZQUIERDA:
-  {
-    DoblarIzq(90);
-    movimiento = POST_DOBLAR;
-
-    break;
-  }
-
-  case CALLEJON:
-  {
-    DoblarIzq(180);
-    movimiento = POST_DOBLAR;
-
-    break;
-  }
-
-  case POST_DOBLAR:
-  {
-    Stop();
-    delay(TICK_STOP);
-    if(distancia_derecha < DISTANCIA_LADOS && distancia_izquierda < DISTANCIA_LADOS) movimiento = PASILLO;
-    else Forward();
-    
-    break;
-  }
-
-  case ANT_DOBLAR:
-  {
-    Stop();
-    delay(TICK_STOP);
-    Forward();
-    delay(TICK_FORWARD);
-    movimiento = DESVIO_IZQUIERDA;
-   
-    break;
-  }
-  }
-}*/
-bool a = 1;
-void setup() {
-  Serial.begin(9600);
-  Serial.println("setup");
-  
+  Led->setPrenderLed();
+}
+void LedOff()
+{
+  Led->setApagarLed();
+}
+void setup()
+{
+   Serial.begin(9600); 
 }
 
-void loop() {
-  if(a){
-    DoblarIzq(3);
-    a = 0;
-    }
-  else {
-    Stop();
-    Serial.println("Stop");
-    }
-  
-  
+void loop()
+{
+bool boton_start = start->getIsPress();
 
-} 
+if (boton_start)
+{
+    LedOn();
+    Serial.print("si");
+}
+else
+{
+    LedOff();
+    Serial.print("no");
+}
+Serial.print(boton_start);
+}*/
