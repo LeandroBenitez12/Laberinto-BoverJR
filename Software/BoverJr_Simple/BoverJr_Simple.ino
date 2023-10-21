b775c04acf255f78e3b873a16a913288ca626e22
LeandroBenitez12 <LeandroBenitez12@users.noreply.github.com>
2023-10-21 00:34:13 -0300

    DEBO SEGUIR CON EL MENU

--- Software/BoverJr_Simple/BoverJr_Simple.ino
+++ Software/BoverJr_Simple/BoverJr_Simple.ino
@@ -10,16 +10,18 @@
 
 BluetoothSerial SerialBT;
 
-//debug
+// debug
 #define TICK_DEBUG_ALL 1000
 #define DEBUG_BUTTON 0
 #define DEBUG_STATUS 0
 #define DEBUG_SENSORS 0
 #define DEBUG_PID 0
+#define TICK_MENU 5000
 unsigned long currentTimePID = 0;
 unsigned long currentTimeDebugAll = 0;
+unsigned long currentTimeMenu = 0;
 
-//Motores
+// Motores
 #define PIN_RIGHT_ENGINE_IN1 26
 #define PIN_RIGHT_ENGINE_IN2 27
 #define PIN_LEFT_ENGINE_IN1 18
@@ -29,7 +31,7 @@ unsigned long currentTimeDebugAll = 0;
 #define PWM_CHANNEL_LEFT_IN1 3
 #define PWM_CHANNEL_LEFT_IN2 4
 
-//Sharps
+// Sharps
 #define PIN_SHARP_RIGHT 25
 #define PIN_SHARP_LEFT 35
 #define PIN_SHARP_FRONT 33
@@ -41,7 +43,7 @@ float frontDistance;
 #define MAX_SIDE_DISTANCE_T 28
 #define MAX_DISTANCE_ANT_TURN 26
 
-//veocidades motores pwm
+// veocidades motores pwm
 #define VELOCIDAD_GIROS_90 247
 #define GIROS_90_DELAY 180
 #define GIROS_180_DELAY 400
@@ -54,7 +56,7 @@ int speedLeftPID = 190;
 int averageSpeedRight = 210;
 int averageSpeedLeft = 190;
 
-//variables pid
+// variables pid
 double kp = 1;
 double kd = 0.14;
 double ki = 0.14;
@@ -62,7 +64,7 @@ double setPoint;
 float gananciaPID;
 double TICK_PID = 20;
 
-//Boton
+// Boton
 #define PIN_BUTTON_START 32
 bool start = 0;
 bool wall;
@@ -78,7 +80,7 @@ Pid *PID = new Pid(kp, kd, ki, setPoint, TICK_PID);
 Button *buttonStart1 = new Button(PIN_BUTTON_START);
 void SensorsRead()
 {
-  
+
   frontDistance = SharpFront->SensorRead();
   rightDistance = SharpRigh->SensorRead();
   leftDistance = SharpLeft->SensorRead();
@@ -86,7 +88,8 @@ void SensorsRead()
 
 bool stateStartButton;
 
-void printButton(){
+void printButton()
+{
   SerialBT.print("Button Start: ");
   SerialBT.println(stateStartButton);
   SerialBT.println("__");
@@ -115,34 +118,39 @@ void printSensors()
   SerialBT.print(rightDistance);
   SerialBT.print(" || leftDistance: ");
   SerialBT.println(leftDistance);
-
 }
 
-void turnRight(){
+void turnRight()
+{
   Bover->Right(VELOCIDAD_GIROS_90, VELOCIDAD_GIROS_90);
   delay(GIROS_90_DELAY);
 }
 
-void turnLeft(){
+void turnLeft()
+{
   Bover->Left(VELOCIDAD_GIROS_90, VELOCIDAD_GIROS_90);
   delay(GIROS_90_DELAY);
 }
 
-void postTurn(){
+void postTurn()
+{
   Bover->Forward(210, 190);
   delay(ENTRAR_EN_PASILLO);
 }
-void antTurn(){
+void antTurn()
+{
   Bover->Forward(VELOCIDAD_GIROS_90, VELOCIDAD_GIROS_90);
   delay(TICK_ANT_TURN);
 }
 
-void fullTurn(){
+void fullTurn()
+{
   Bover->Right(VELOCIDAD_GIROS_90, VELOCIDAD_GIROS_90);
   delay(GIROS_180_DELAY);
-} 
+}
 
-void ignoreTurn(){
+void ignoreTurn()
+{
   Bover->Forward(VELOCIDAD_GIROS_90, VELOCIDAD_GIROS_90);
   delay(ENTRAR_EN_PASILLO);
 }
@@ -168,15 +176,15 @@ void movementLogic()
   {
     bool stateStartButton = buttonStart1->GetIsPress();
     Bover->Stop();
-    //SerialBT.print("Seguir(0 = IZQ || 1 = DER):   ");
-    //SerialBT.println(wall);
-    //wall = SerialBT.read();
-    //SerialBT.print("INICIAR: ");
-    //bool stateStartBluetooh = SerialBT.read();
-    if (stateStartButton) {
+    // SerialBT.print("Seguir(0 = IZQ || 1 = DER):   ");
+    // SerialBT.println(wall);
+    // wall = SerialBT.read();
+    // SerialBT.print("INICIAR: ");
+    // bool stateStartBluetooh = SerialBT.read();
+    if (stateStartButton)
+    {
       delay(1000);
       movement = CONTINUE;
-    
     }
     break;
   }
@@ -185,26 +193,28 @@ void movementLogic()
   {
     float input = rightDistance - leftDistance;
     gananciaPID = PID->ComputePid(input);
-    if(DEBUG_PID) printPID();
+    if (DEBUG_PID)
+      printPID();
     speedRightPID = (averageSpeedRight + (gananciaPID));
     speedLeftPID = (averageSpeedLeft - (gananciaPID));
     Bover->Forward(speedRightPID, speedLeftPID);
-    
-    if (frontDistance < MAX_FRONT_DISTANCE) movement = STOP;
-    if (rightDistance > MAX_SIDE_DISTANCE )
-      { 
-        Bover->Forward(averageSpeedRight, averageSpeedLeft);
-        delay(TICK_ANT_TURN);
-        movement = RIGHT_TURN;
-      }
+
+    if (frontDistance < MAX_FRONT_DISTANCE)
+      movement = STOP;
+    if (rightDistance > MAX_SIDE_DISTANCE)
+    {
+      Bover->Forward(averageSpeedRight, averageSpeedLeft);
+      delay(TICK_ANT_TURN);
+      movement = RIGHT_TURN;
+    }
     /*if (rightDistance > MAX_SIDE_DISTANCE && frontDistance > MAX_DISTANCE_ANT_TURN && leftDistance < MAX_SIDE_DISTANCE ) movement = RIGHT_TURN;
     if (leftDistance > MAX_SIDE_DISTANCE && frontDistance > MAX_DISTANCE_ANT_TURN && rightDistance < MAX_SIDE_DISTANCE) movement = IGNORE_TURN;
-    
+
     else if(wall == false){
       if (rightDistance > MAX_SIDE_DISTANCE && frontDistance > MAX_DISTANCE_ANT_TURN && leftDistance < MAX_SIDE_DISTANCE ) movement = IGNORE_TURN;
       if (leftDistance > MAX_SIDE_DISTANCE && frontDistance > MAX_DISTANCE_ANT_TURN && rightDistance < MAX_SIDE_DISTANCE) movement = ANT_TURN;
     }
-    */ 
+    */
     break;
   }
 
@@ -212,25 +222,31 @@ void movementLogic()
   {
     Bover->Stop();
     delay(STOPS_PARA_PENSAR);
-    if (frontDistance < MAX_FRONT_DISTANCE){
-      if (rightDistance > MAX_SIDE_DISTANCE && leftDistance <= MAX_SIDE_DISTANCE) movement = RIGHT_TURN;
-      if (rightDistance <= MAX_SIDE_DISTANCE && leftDistance > MAX_SIDE_DISTANCE) movement = LEFT_TURN;
-      
-      if (rightDistance > MAX_SIDE_DISTANCE_T && leftDistance > MAX_SIDE_DISTANCE_T) movement = RIGHT_TURN;
-      else  {
-              Bover->Right(VELOCIDAD_GIROS_90, VELOCIDAD_GIROS_90);
-              delay(60);
-              movement = CONTINUE;
-            }
+    if (frontDistance < MAX_FRONT_DISTANCE)
+    {
+      if (rightDistance > MAX_SIDE_DISTANCE && leftDistance <= MAX_SIDE_DISTANCE)
+        movement = RIGHT_TURN;
+      if (rightDistance <= MAX_SIDE_DISTANCE && leftDistance > MAX_SIDE_DISTANCE)
+        movement = LEFT_TURN;
+
+      if (rightDistance > MAX_SIDE_DISTANCE_T && leftDistance > MAX_SIDE_DISTANCE_T)
+        movement = RIGHT_TURN;
+      else
+      {
+        Bover->Right(VELOCIDAD_GIROS_90, VELOCIDAD_GIROS_90);
+        delay(60);
+        movement = CONTINUE;
+      }
       /*{
-         
+
         if(wall == true) movement = LEFT_TURN;
         else if (wall == false) movement = RIGHT_TURN;
       }
       if (rightDistance <= MAX_SIDE_DISTANCE && leftDistance <= MAX_SIDE_DISTANCE) movement = FULL_TURN;
       */
     }
-    else movement = CONTINUE;
+    else
+      movement = CONTINUE;
     break;
   }
 
@@ -287,130 +303,177 @@ void movementLogic()
     movement = CONTINUE;
     break;
   }
-
+  }
 }
-}
 
-void printStatus(){
+void printStatus()
+{
   String state = "";
   switch (movement)
   {
-    case STANDBY: state = "STANDBY";
+  case STANDBY:
+    state = "STANDBY";
     break;
-    case CONTINUE: state = "CONTINUE";
+  case CONTINUE:
+    state = "CONTINUE";
     break;
-    case STOP: state = state = "STOP"; 
+  case STOP:
+    state = state = "STOP";
     break;
-    case RIGHT_TURN: state = "RIGHT TURN"; 
+  case RIGHT_TURN:
+    state = "RIGHT TURN";
     break;
-    case LEFT_TURN: state = "LEFT TURN"; 
+  case LEFT_TURN:
+    state = "LEFT TURN";
     break;
-    case FULL_TURN: state = "FULL TURN"; 
+  case FULL_TURN:
+    state = "FULL TURN";
     break;
-    case POST_TURN: state = "POST TURN";
+  case POST_TURN:
+    state = "POST TURN";
     break;
-    case ANT_TURN: state = "ANT TURN";
+  case ANT_TURN:
+    state = "ANT TURN";
     break;
-    case IGNORE_TURN: state = "IGNORE_TURN";
+  case IGNORE_TURN:
+    state = "IGNORE_TURN";
     break;
   }
   SerialBT.print("State: ");
   SerialBT.println(state);
 }
 
-enum OpcionSeleccionada
+enum menu
 {
-  MENU_OPCIONES,
+  ELECCION,
   KP,
   KI,
   KD,
   VEL_DERECHA,
   VEL_IZQUIERDA,
   DEBUGEAR_TODO,
-  NADA
+  SALIR
 };
-int mensaje = MENU_OPCIONES;
+int menu = ELECCION;
 
-void menuConfig(){
-  char OpcionSeleccionada = SerialBT.read();
-  switch (OpcionSeleccionada)
+void menuConfig()
+{
+  String opcionSeleccionada = "";
+  switch (menu)
   {
-    case MENU_OPCIONES:
+  case ELECCION:
+  {
+    opcionSeleccionada = SerialBT.read();
+    if (opcionSeleccionada == "A")
+      menu = KP;
+    if (opcionSeleccionada == "B")
+      menu = KI;
+    if (opcionSeleccionada == "C")
+      menu = KD;
+    if (opcionSeleccionada == "D")
+      menu = VEL_DERECHA;
+    if (opcionSeleccionada == "E")
+      menu = VEL_IZQUIERDA;
+    if (opcionSeleccionada == "F")
+      menu = DEBUGEAR_TODO;
+    if (opcionSeleccionada == "G")
+      menu = SALIR;
+  }
+  case KP:
+  {
+    int dato = SerialBT.read();
+    kp = dato;
+    menu = SALIR;
+  }
+  break;
+  case KI:
+  {
+    int dato = SerialBT.read();
+    ki = dato;
+    menu = SALIR;
+  }
+  break;
+  case KD:
+  {
+    int dato = SerialBT.read();
+    kd = dato;
+    menu = SALIR;
+  }
+  break;
+  case VEL_DERECHA:
+  {
+    int dato = SerialBT.read();
+    speedRightPID = dato;
+    menu = SALIR;
+  }
+  break;
+  case VEL_IZQUIERDA:
+  {
+    int dato = SerialBT.read();
+    speedLeftPID = dato;
+    menu = SALIR;
+  }
+  break;
+  case DEBUGEAR_TODO:
+  {
+    /*bool dato = SerialBT.read();
+    dato = DEBUG;
+    DEBUG_PID = dato;
+    DEBUG_SENSORS = dato;
+    DEBUG_STATUS = dato;
+    */
+    menu = SALIR;
+  }
+  break;
+  case SALIR:
+    SerialBT.println("SALIR");
+    break;
+  }
+}
+
+void printAll()
+{
+  if (millis() > currentTimeDebugAll + TICK_DEBUG_ALL)
+  {
+    currentTimeDebugAll = millis();
+    if (DEBUG_BUTTON)
+      printButton();
+    if (DEBUG_SENSORS)
+      printSensors();
+    SerialBT.println("__");
+    if (DEBUG_STATUS)
+      printStatus();
+    SerialBT.println("_______");
+  }
+}
+void setup()
+{
+  SerialBT.begin("Bover");
+
+  // pinMode(PIN_BUTTON_START, INPUT_PULLUP);
+}
+
+void loop()
+{
+  if (SerialBT.available())
+  {
+    menuConfig();
+    SensorsRead();
+    movementLogic();
+  }
+  else
+  {
+    if (millis() > currentTimeMenu + TICK_MENU)
+    {
+      currentTimeMenu = millis();
       SerialBT.println("A. Cambiar Kp ");
       SerialBT.println("B. Cambiar Ki ");
       SerialBT.println("C. Cambiar Kd ");
       SerialBT.println("D. Cambiar Vel Derecha ");
       SerialBT.println("E. Cambiar Vel Izquierda ");
       SerialBT.println("F. Debugear todo ");
-      SerialBT.println("G. NADA ");
-
-    break;
-    case KP: if (OpcionSeleccionada == "A") {
-      int dato  = SerialBT.read();
-      kp = dato;
-      }
-    break;
-    case KI: if (OpcionSeleccionada == "B") {
-      int dato  = SerialBT.read();
-      ki  = dato;
-      }
-    break;
-    case KD: if (OpcionSeleccionada == "C") {
-      int dato  = SerialBT.read();
-      kd = dato;
-      }
-    break;
-    case VEL_DERECHA: if (OpcionSeleccionada == "D") {
-      int dato  = SerialBT.read(); 
-      speedRightPID = dato;
-      }
-    break;
-    case VEL_IZQUIERDA: if (OpcionSeleccionada == "E") {
-      int dato  = SerialBT.read();
-      speedLeftPID = dato;
-      }
-    break;
-    case DEBUGEAR_TODO: if (OpcionSeleccionada == "F") {
-      bool dato  = SerialBT.read();
-      DEBUG_BUTTON = dato;
-      DEBUG_PID = dato;
-      DEBUG_SENSORS = dato;
-      DEBUG_STATUS = dato;
-      }
-    break;
-    case NADA : 
-      SerialBT.println("nada");
-    break;
+      SerialBT.println("G. SALIR ");
+    }
   }
-  SerialBT.print("State: ");
-  SerialBT.println(state);
-}
-
-void printAll(){
-  if (millis() > currentTimeDebugAll + TICK_DEBUG_ALL)
-  {
-    currentTimeDebugAll = millis();
-    if(DEBUG_BUTTON) printButton();
-    if (DEBUG_SENSORS) printSensors();
-    SerialBT.println("__");
-    if (DEBUG_STATUS) printStatus();
-    SerialBT.println("_______");
-  }
-}
-void setup() 
-{
-  //SerialBT.begin("Bover");
-  //pinMode(PIN_BUTTON_START, INPUT_PULLUP);
-}
-
-void loop() 
-{    
-  if(SerialBT.available()){
-    menuConfig();
-    SensorsRead();
-    movementLogic();
-
-  }
-  
-  //printAll();
+  // printAll();
 }
--- Software/Librerias/PID/PID.h
+++ Software/Librerias/PID/PID.h
@@ -7,17 +7,19 @@ class Pid
 private:
     double kp = 0;
     double kd = 0;
+    double ki = 0;
     double error;
     double lastError;
     double Input = 0;
     double output = 0;
     double setPoint = 0;
     double deltaError;
+    double integral;
     unsigned long currentTimePID = 0;
     int TICK_PID = 70;
 
 public:
-    Pid(double p, double d, double sp, double tick);
+    Pid(double p, double d, double i, double sp, double tick);
     double ComputePid(double inp);
 };
 #endif
