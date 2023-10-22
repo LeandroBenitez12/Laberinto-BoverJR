505365b5461c697e0551bf94bbe2b46c8b9ba7c2
LeandroBenitez12 <LeandroBenitez12@users.noreply.github.com>
2023-10-22 19:19:00 -0300

    merge

--- Software/Librerias/PID/PID.h
+++ Software/Librerias/PID/PID.h
@@ -7,11 +7,7 @@ class Pid
 private:
     double kp = 0;
     double kd = 0;
-<<<<<<< HEAD
     double ki = 0;
-=======
-    double ki= 0;
->>>>>>> d0b76f3ee8112b5f2127c17d0ac5584b443cd0dd
     double error;
     double lastError;
     double Input = 0;
@@ -23,11 +19,7 @@ private:
     int TICK_PID = 70;
 
 public:
-<<<<<<< HEAD
     Pid(double p, double d, double i, double sp, double tick);
-=======
-    Pid(double p, double d,double i , double sp, double tick);
->>>>>>> d0b76f3ee8112b5f2127c17d0ac5584b443cd0dd
     double ComputePid(double inp);
 };
 #endif
