6c57df45090c78190ac75c893818a7832109e972
LeandroBenitez12 <LeandroBenitez12@users.noreply.github.com>
2023-10-27 10:04:18 -0300

    sobreescirbo

--- Software/MPU6050/MPU6050.ino
+++ Software/MPU6050/MPU6050.ino
@@ -41,31 +41,33 @@ MPU6050 mpu;
 // MPU6050 mpu(0x69); // <-- use for AD0 high
 
 // MPU control/status vars
-bool dmpReady = false;   // set true if DMP init was successful
-uint8_t mpuIntStatus;    // holds actual interrupt status byte from MPU
-uint8_t devStatus;       // return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;     // expected DMP packet size (default is 42 bytes)
-uint16_t fifoCount;      // count of all bytes currently in FIFO
-uint8_t fifoBuffer[64];  // FIFO storage buffer
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-Quaternion q;         // [w, x, y, z]
-VectorInt16 aa;       // [x, y, z]
-VectorInt16 aaReal;   // [x, y, z]
-VectorInt16 aaWorld;  // [x, y, z]
-VectorFloat gravity;  // [x, y, z]
-float ypr[3];         // [yaw, pitch, roll]
+Quaternion q;        // [w, x, y, z]
+VectorInt16 aa;      // [x, y, z]
+VectorInt16 aaReal;  // [x, y, z]
+VectorInt16 aaWorld; // [x, y, z]
+VectorFloat gravity; // [x, y, z]
+float ypr[3];        // [yaw, pitch, roll]
 
 volatile bool mpuInterrupt = false;
-void dmpDataReady() {
+void dmpDataReady()
+{
   mpuInterrupt = true;
 }
 
-void mpuSetup() {
+void mpuSetup()
+{
 
 // join I2C bus (I2Cdev library doesn't do this automatically)
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
   Wire.begin();
-  Wire.setClock(400000);  // 400kHz I2C clock. Comment this line if having compilation difficulties
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
 #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
   Fastwire::setup(400, true);
 #endif
@@ -92,7 +94,8 @@ void mpuSetup() {
   mpu.setZAccelOffset(1063);
 
   // Activar DMP
-  if (devStatus == 0) {
+  if (devStatus == 0)
+  {
     SerialBT.println(F("Enabling DMP..."));
     mpu.setDMPEnabled(true);
 
@@ -105,7 +108,9 @@ void mpuSetup() {
 
     // get expected DMP packet size for later comparison
     packetSize = mpu.dmpGetFIFOPacketSize();
-  } else {
+  }
+  else
+  {
     // ERROR!
     // 1 = initial memory load failed
     // 2 = DMP configuration updates failed
@@ -115,13 +120,15 @@ void mpuSetup() {
     SerialBT.println(F(")"));
   }
 }
-void mpuLoop() {
+void mpuLoop()
+{
   // Si fallo al iniciar, parar programa
   if (!dmpReady)
     return;
 
   // Ejecutar mientras no hay interrupcion
-  while (!mpuInterrupt && fifoCount < packetSize) {
+  while (!mpuInterrupt && fifoCount < packetSize)
+  {
     // AQUI EL RESTO DEL CODIGO DE TU PROGRRAMA
   }
 
@@ -132,10 +139,13 @@ void mpuLoop() {
   fifoCount = mpu.getFIFOCount();
 
   // Controlar overflow
-  if ((mpuIntStatus & 0x10) || fifoCount == 4096) {
+  if ((mpuIntStatus & 0x10) || fifoCount == 4096)
+  {
     mpu.resetFIFO();
     SerialBT.println(F("FIFO overflow!"));
-  } else if (mpuIntStatus & 0x02) {
+  }
+  else if (mpuIntStatus & 0x02)
+  {
     // wait for correct available data length, should be a VERY short wait
     while (fifoCount < packetSize)
       fifoCount = mpu.getFIFOCount();
@@ -152,23 +162,25 @@ void mpuLoop() {
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
     gyroZ = ypr[0] * 180 / M_PI;
-    if (millis() > currentTimeZ + TICK_DEBUG_Z) {
+    if (millis() > currentTimeZ + TICK_DEBUG_Z)
+    {
       currentTimeZ = millis();
       SerialBT.print("Eje Z");
       SerialBT.print(gyroZ);
     }
     delay(1);
   }
-}
-void setup() {
-  mpuSetup();
-}
+  void setup()
+  {
+    mpuSetup();
+  }
 
-void loop() {
+  void loop()
+  {
 
-  mpuLoop();
-  // Controlar el giro del robot hacia 90 grados
-  /*if (gyroZ > 105) {
+    mpuLoop();
+    // Controlar el giro del robot hacia 90 grados
+    /*if (gyroZ > 105) {
         // Girar a la izquierda
         SerialBT.println("HACIA LA IZQUIERDA  ");
         robot->Left(220, 220);
@@ -182,19 +194,26 @@ void loop() {
         SerialBT.println("DETENIDO");
     }
     */
-  if (gyroZ >= 0 && gyroZ <= 180) {
-    do {
-      robot->Right(160, 160);
-      if (millis() > currentTimeDer + TICK_DEBUG_Z) {
-        currentTimeDer = millis();
-        SerialBT.println("HACIA LA DERECHA  ");
-      }
-    } while (gyroZ > -180 && gyroZ < 0);
-  } else {
-    robot->Stop();
-    if (millis() > currentTimeStop + TICK_DEBUG_Z) {
-      currentTimeStop = millis();
-      SerialBT.println("detenido  ");
+    if (gyroZ >= 0 && gyroZ <= 180)
+    {
+      do
+      {
+        robot->Right(160, 160);
+        if (millis() > currentTimeDer + TICK_DEBUG_Z)
+        {
+          currentTimeDer = millis();
+          SerialBT.println("HACIA LA DERECHA  ");
+        }
+      } while (gyroZ > -180 && gyroZ < 0);
     }
-}
+    else
+    {
+      robot->Stop();
+      if (millis() > currentTimeStop + TICK_DEBUG_Z)
+      {
+        currentTimeStop = millis();
+        SerialBT.println("detenido  ");
+      }
+    }
+  }
 }
